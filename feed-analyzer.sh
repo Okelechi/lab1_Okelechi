675214e1cd75b19eb8e231ad05b3e67d8d1c793c
Okelechi <k.oparaji@alustudent.com>
2026-05-22 22:32:29 +0400

    completed a shell script that extracts username, sorts, counts and sorts according to highest number and prints the top 5 users

--- feed-analyzer.sh
+++ feed-analyzer.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+ba#!/bin/bash
 
-awk '/^[0-9]/ {printf("\n%s",$0)} {printf(" %s", $0)}' "$1" | cut -d',' -f2 | tail -n +2 | sort | uniq -c | sort -rn | head -5 |awk '{print($2)}'
+awk '/^[0-9]/ {printf("\n%s",$0)} {printf(" %s", $0)}' "$1" | cut -d',' -f2 | tail -n +2 | sort | uniq -c | sort -rn | head -5 
